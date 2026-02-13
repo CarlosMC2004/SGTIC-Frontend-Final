a5b78b84710650d6013bb43ba977a6fe3ce866e3
CarlosMC2004 <cmanotoaa@uteq.edu.ec>
2026-02-13 00:24:06 -0500

    rutas corregidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-=======
-import { InstitutionalStructureComponent } from './pages/administrator/institutional-structure/institutional-structure';
->>>>>>> 3b8c420c2e7e916c77ce1b9f5a8d40eee3c249d8
 import { LoginComponent } from './pages/general/login/login';
 import { MenuPrincipalAdminComponent } from './pages/administrator/main-menu/main-menu';
 import {AcademicConfiguration} from './pages/administrator/academic-configuration/academic-configuration';
@@ -15,17 +11,12 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     //RUTA MENU ADMINISTRADOR
     { path: 'main-menu', component: MenuPrincipalAdminComponent },
-    //RUTA DE CONFIGURACION DE ADMINISTRADOR
-<<<<<<< HEAD
-    { path: 'AcademicConfiguration', component: AcademicConfiguration },
     //RUTA DE ESTRUCTURA INSTITUCIONAL
     { path: 'institutional-structure', component: InstitutionalStructureComponent},
     //RUTA PARA AGREGAR NUEVA FACULTAD
     { path: 'add-capability', component: FacultyAdminComponent},
     //RUTA PARA AGREGAR NUEVA CARRERA
     { path: 'add-career', component: CareerAdminComponent},
-=======
-    {path: 'academic-configuration', component: AcademicConfiguration},
-    { path: 'institutional-structure', component: InstitutionalStructureComponent },
->>>>>>> 3b8c420c2e7e916c77ce1b9f5a8d40eee3c249d8
+    //RUTA DE CONFIGURACION DE ADMINISTRADOR
+    { path: 'academic-configuration', component: AcademicConfiguration},
 ];
