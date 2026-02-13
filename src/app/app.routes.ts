3b8c420c2e7e916c77ce1b9f5a8d40eee3c249d8
luis paca <lpaca99@gmail.com>
2026-02-13 00:12:59 -0500

    corregir main-menu a gusto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,5 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { AdminDashboardComponent } from './pages/administrator/main-menu/main-menu';
 import { InstitutionalStructureComponent } from './pages/administrator/institutional-structure/institutional-structure';
-
-export const routes: Routes = [
-    { path: 'main-menu', component: AdminDashboardComponent },
-    { path: 'institutional-structure', component: InstitutionalStructureComponent },
-];
-=======
 import { LoginComponent } from './pages/general/login/login';
 import {MenuPrincipalAdminComponent} from './pages/administrator/main-menu/main-menu';
 import {AcademicConfiguration} from './pages/administrator/academic-configuration/academic-configuration';
@@ -18,6 +10,6 @@ export const routes: Routes = [
     //RUTA MENU ADMINISTRADOR
     {path: 'main-menu', component: MenuPrincipalAdminComponent},
     //RUTA DE CONFIGURACION DE ADMINISTRADOR
-    {path: 'academic-configuration', component: AcademicConfiguration}
+    {path: 'academic-configuration', component: AcademicConfiguration},
+    { path: 'institutional-structure', component: InstitutionalStructureComponent },
 ];
->>>>>>> 7073174a95fa2c9626a2ed1edddfa987ee1d8594
