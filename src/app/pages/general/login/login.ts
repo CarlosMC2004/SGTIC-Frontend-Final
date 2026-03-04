308ef423b7e5c36216a10a15277c54d7b9cf3c58
Roger <steveto0090@gmail.com>
2026-03-04 05:30:44 -0500

    Cambios login

--- src/app/pages/general/login/login.ts
+++ src/app/pages/general/login/login.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 import { AuthLayoutComponent } from '../../../components/auth-layout/auth-layout';
+import { SolicitudIngresoModalComponent } from '../../../components/components-studients/modal-solicitar-acceso/modal-solicitar-acceso'
+import { RequestAccess } from '../../../services/request-access/request-access';
+import { RequestAccessDTO } from '../../../services/request-access/request-access';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterLink, AuthLayoutComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, AuthLayoutComponent, SolicitudIngresoModalComponent],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
@@ -17,13 +20,14 @@ export class LoginComponent {
   showPassword = false;
   isLoading = false;
   errorMessage = '';
+  isModalOpen = false;
+  private requestAccess = inject(RequestAccess);
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private authService: AuthService
   ) {
-    // CAMBIO: 'username' -> 'email' para coincidir con el backend
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
@@ -37,30 +41,25 @@ export class LoginComponent {
   onSubmit() {
     this.errorMessage = '';
 
-    // 1. Validación del formulario
     if (this.loginForm.invalid) {
       this.loginForm.markAllAsTouched();
       return;
     }
 
-    // 2. Activamos el estado de carga
     this.isLoading = true;
 
-    // 3. Petición al backend
     this.authService.login(this.loginForm.value).subscribe({
       next: (response) => {
         this.isLoading = false;
 
-        // ---> VALIDACIÓN DE PRIMER INGRESO <---
         if (response.primerIngreso === false) {
           this.router.navigate(['/change-password']).catch(err => {
             console.error('Error de enrutamiento: ¿Ya registraste /hoja-de-vida en tus rutas?', err);
             this.errorMessage = 'Error interno: La ruta de destino no existe.';
           });
-          return; // Detiene la ejecución aquí
+          return;
         }
 
-        // ---> REDIRECCIÓN BASADA EN ROLES <---
         const roles = response.roles;
 
         if (roles.includes('administrador_sgtic')) {
@@ -68,7 +67,7 @@ export class LoginComponent {
         } else if (roles.includes('coordinador_facultad')) {
           this.router.navigate(['/coordinator/faculty']);
         } else if (roles.includes('coordinador_carrera')) {
-          this.router.navigate(['/coordinator/career']);
+          this.router.navigate(['/coordinator/StudentRequests']);
         } else if (roles.includes('docente') || roles.includes('director_trabajo_titulacion')) {
           this.router.navigate(['/teacher/dashboard']);
         } else if (roles.includes('estudiante')) {
@@ -78,9 +77,9 @@ export class LoginComponent {
         }
       },
       error: (error) => {
-        // 4. Manejo de errores
+
         this.isLoading = false;
-        
+
         if (error.status === 401 || error.status === 403) {
           this.errorMessage = 'Credenciales incorrectas o usuario inactivo';
         } else if (error.status === 0) {
@@ -88,6 +87,27 @@ export class LoginComponent {
         } else {
           this.errorMessage = 'Error al iniciar sesión. Intente nuevamente.';
         }
+      }
+    });
+  }
+  openModal(event: Event) {
+    event.preventDefault();
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  handleRequestAccessSubmission(data: RequestAccessDTO) {
+    this.requestAccess.enviarSolicitud(data).subscribe({
+      next: (response) => {
+        alert('Solicitud enviada exitosamente. Nos pondremos en contacto contigo pronto.');
+        this.closeModal();
+      },
+      error: (err) => {
+        const errorMsg = err.error?.error || 'Un error ocurrió al enviar la solicitud. Intente nuevamente.';
+        alert(errorMsg);
       }
     });
   }
